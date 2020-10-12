f4c37b1bd3b1e6e90a6deb3f3de61dcd5581ced0
root <ashkanebtekari@gmail.com>
2020-10-12 10:14:45 -0400

    new post logger

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,18 @@ class PostsController < ApplicationController
 
   # New Post
   def new
+    @new = 'we show new things'
+  end
+
+  # Show Posts
+  def show
 
   end
+
+  # Create Post
+  def create
+    render plain: params[:post].inspect
+  end
+
+
 end
