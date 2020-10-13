ee8cb83d0fc67bbfaa9bf3f5f78d95b1e87fe70c
root <ashkanebtekari@gmail.com>
2020-10-13 04:24:28 -0400

    generated migrate file

---
+++ db/migrate/20201013081840_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :firstname
+      t.string :lastname
+      t.string :email
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
