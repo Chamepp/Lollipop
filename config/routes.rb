1cbd94d11b911940602bf483b44fce40dfa1b806
root <ashkanebtekari@gmail.com>
2020-10-15 10:02:50 -0400

    added new signup route to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
 
   # Additional Pages
   get 'showall' => 'posts#showall', as: 'showall'
-  get 'signup' => 'pages#signup', as: 'signup'
   get 'about' => 'pages#about', as: 'about'
+  get 'signup' => 'user#signup', as: 'signup'
+  post 'users' => 'users#makeuser'
 
   # Resources
   resources :posts
