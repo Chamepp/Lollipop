a2b408fe0fa1a84bd3b166b8198fccc028be7181
root <ashkanebtekari@gmail.com>
2020-10-16 12:07:54 -0400

    updated the page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,27 @@ class PagesController < ApplicationController
 
   end
 
+  # Create User
+  def create
+    user = User.new(
+      firstname: params[:firstname],
+      lastname: params[:lastname],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+    )
+
+    if user.save
+      session[:user_id] = user.id
+      flash[:success] = "Successfully Created The User ..."
+      redirect_to home_path
+
+    else
+      flash[:warning] = "Invalid Email or Password"
+      redirect_to signup_path
+    end
+  end
+
   # AboutUs Page
   def about
 
