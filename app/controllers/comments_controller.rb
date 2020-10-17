99a405178012a3b7e46b163967490ca5c8c4b12e
root <ashkanebtekari@gmail.com>
2020-10-17 13:42:46 -0400

    added commenting ability

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+
+
+  # Create Comments
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
+  end
+
+
+  # Comments Params
+  def comment_params
+    params.require(:comment).permit(:username, :body)
+  end
+
 end
